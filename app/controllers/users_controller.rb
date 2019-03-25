dec9b317425e40baa6747628d3158ead6edf5b70
Sara Walker <sarawall1187@gmail.com>
2019-03-25 21:09:53 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,17 +20,17 @@ class UsersController < ApplicationController
       end
 
       get '/login' do
-        # if !logged_in?
+        if !logged_in?
           erb :'/users/login'
-        # else
-        #   redirect to '/tweets'
-        # end
+        else
+          redirect to '/tweets'
+        end
       end
 
       post '/login' do
-        @user = User.find_by(params[:username])
+        @user = User.find_by(:username => params[:username])
         @user.authenticate(params[:password])
-        if @user
+      if @user
         session[:user_id] = @user.id
         redirect to '/tweets'
       else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
+
+  def slug
+  self.username.gsub(/[^0-9A-Za-z]/, '-').downcase
+end
+
+def self.find_by_slug(slug)
+  self.all.find {|user| user.slug == slug}
+end
 end
