823f15e2b9ee54f00f2a5d68f929d6dd5fd776c5
Sara Walker <sarawall1187@gmail.com>
2019-03-26 17:43:16 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -60,7 +60,7 @@ class TweetsController < ApplicationController
      end
    end
 
-    delete '/tweets/:id/delete' do 
+    delete '/tweets/:id/delete' do
       @tweet = Tweet.find(params[:id])
       @tweet.delete
       redirect to '/tweets'
